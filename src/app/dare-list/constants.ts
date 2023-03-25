04d3b8412a1af46778466959dc5088c2007be6cd
Matt Barlow <83474131+mattbarlow-sg@users.noreply.github.com>
2023-03-25 17:11:59 -0400

    add uuids

--- src/app/dare-list/constants.ts
+++ src/app/dare-list/constants.ts
@@ -8,40 +8,104 @@ shaking hands
 punching bag
  */
 export const KNOWN_DARES_LIST = [
-    {
-      key: '871c26d5-d2fe-4a47-99ab-904b773ad708',
-      keyPrompt: 'Headbang For 10 Seconds',
-      acceptableTokens: 'headbanging' // read for an array of acceptable tokens
-    },
-    {
-      key: '73fdfc8c-57a0-4b20-b4cc-37086ec5c4cb',
-      keyPrompt: 'Hit the punching bag for 10 seconds',
-      acceptableTokens: 'punching bag'
-    },
-    {
-      key: 'd7f8ad97-8d56-4b58-b000-f8d4f964eea3',
-      keyPrompt: 'Attempt to hack into the Brickyard Network for 10 seconds',
-      acceptableTokens: 'using computer'
-    },
-    {
-      key: '2914c1fd-ba9a-4875-9034-d91f3bdffe4b',
-      keyPrompt: 'KILL bacteria on your Hands for 10 seconds',
-      acceptableTokens: 'washing hands'
-    },
-    {
-        key: 'e87b08d2-f32f-43f5-be07-b1c826e7a2fa',
-        keyPrompt: 'Air drum for 10 seconds',
-        acceptableTokens: 'air drumming'
-    },
-    {
-        key: 'd7e5ef20-456d-41d4-a838-1be64e06a268',
-        keyPrompt: 'Shake someone\'s hand for 10 seconds',
-        acceptableTokens: 'shaking hands'
-    },
-    {
-        key: 'd7f8ad97-8d56-4b58-b000-f8d4f964eea3',
-        keyPrompt: 'Drink beer for 10 seconds',
-        acceptableTokens: 'tasting beer'
-    },
-  ];
-  
+  {
+    key: '871c26d5-d2fe-4a47-99ab-904b773ad708',
+    keyPrompt: 'Headbang For 10 Seconds',
+    acceptableTokens: 'headbanging' // read for an array of acceptable tokens
+  },
+  {
+    key: '73fdfc8c-57a0-4b20-b4cc-37086ec5c4cb',
+    keyPrompt: 'Hit the punching bag for 10 seconds',
+    acceptableTokens: 'punching bag'
+  },
+  {
+    key: 'd7f8ad97-8d56-4b58-b000-f8d4f964eea3',
+    keyPrompt: 'Attempt to hack into the Brickyard Network for 10 seconds',
+    acceptableTokens: 'using computer'
+  },
+  {
+    key: '2914c1fd-ba9a-4875-9034-d91f3bdffe4b',
+    keyPrompt: 'KILL bacteria on your Hands for 10 seconds',
+    acceptableTokens: 'washing hands'
+  },
+  {
+    key: 'e87b08d2-f32f-43f5-be07-b1c826e7a2fa',
+    keyPrompt: 'Air drum for 10 seconds',
+    acceptableTokens: 'air drumming'
+  },
+  {
+    key: 'd7e5ef20-456d-41d4-a838-1be64e06a268',
+    keyPrompt: 'Shake someone\'s hand for 10 seconds',
+    acceptableTokens: 'shaking hands'
+  },
+  {
+    key: 'd7f8ad97-8d56-4b58-b000-f8d4f964eea3',
+    keyPrompt: 'Drink beer for 10 seconds',
+    acceptableTokens: 'tasting beer'
+  },
+  {
+    key: '82886F02-5791-422F-9262-ADF0665ABF15',
+    keyPrompt: 'Jump as high as you can',
+    acceptableTokens: 'high jump'
+  },
+  {
+    key: '8CE941DA-D0B1-4E0E-89FA-C9411738BB40',
+    keyPrompt: 'Clap your hands for 10 seconds',
+    acceptableTokens: 'clapping'
+  },
+  {
+    key: 'A769D82F-D9C2-479A-A72B-9C9E32C91648',
+    keyPrompt: 'Crack your neck',
+    acceptableTokens: 'cracking neck'
+  },
+  {
+    key: '18FE3656-3AFF-41FF-A44F-98480C3FD66D',
+    keyPrompt: 'Headbutt somebody',
+    acceptableTokens: 'headbutting'
+  },
+  {
+    key: '115C09A6-B485-4421-B1BA-82C1FB044F7E',
+    keyPrompt: 'Bust out some parkour moves',
+    acceptableTokens: 'parkour'
+  },
+  {
+    key: 'A19C71D7-AA9A-42BA-B213-EA6D30E28D47',
+    keyPrompt: 'Cry like it is the worst day of your life',
+    acceptableTokens: 'crying'
+  },
+  {
+    key: 'B44EFC0A-174E-49BA-AAE6-01DA47FBEE38',
+    keyPrompt: 'Show your best belly dance',
+    acceptableTokens: 'belly dancing'
+  },
+  {
+    key: 'B64D1A8C-86D0-4CAB-B701-43DB008962B6',
+    keyPrompt: 'March around like a soldier',
+    acceptableTokens: 'marching'
+  },
+  {
+    key: '74EA6763-39D0-4BDD-AC75-ED568C396CAD',
+    keyPrompt: 'Brush your hair for 10 seconds',
+    acceptableTokens: 'brushing hair'
+  },
+  {
+    key: 'D26DAFFB-2045-4DD0-BD05-CB41A8CFD9E4',
+    keyPrompt: 'Yawn like you just got out of bed',
+    acceptableTokens: 'yawning'
+  },
+  {
+    key: 'D7CD64C3-F33C-4D2B-BE04-FFAD4ED1AF60',
+    keyPrompt: 'Juggle some balls like a clown',
+    acceptableTokens: 'juggling balls'
+  },
+  {
+    key: '90A8BFBF-2E13-4DD8-9937-EFBA2DC7FFC1',
+    keyPrompt: 'Run on the treadmill for 10 seconds',
+    acceptableTokens: 'running on treadmill'
+  },
+  {
+    key: 'F5ED20DB-E346-431C-B683-D7FEEB31E059',
+    keyPrompt: 'Hug someone to show that you care about them',
+    acceptableTokens: 'hugging'
+  },
+];
