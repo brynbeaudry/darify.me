ab161c3a76b442e8f9b4a9f4f0a860856ee44659
Bryn Beaudry <brynbeaudry@Bryns-Intel-MacBook-Pro.local>
2023-03-25 17:58:29 -0400

    m

--- src/app/services/gpt3.service.ts
+++ src/app/services/gpt3.service.ts
@@ -9,12 +9,12 @@ export class Gpt3Service {
   private apiUrl = 'https://api.openai.com/v1/completions';
   private xyN6 = 'c2stQUl2OVJ2TkxNNjFHYW10WWxFa2xUM0JsYmtGSkE5czlWTE5CVEhuRW5yVXBQa3BCCg=='
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   generateText(prompt: string): Observable<any> {
     const headers = {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${Buffer.from(this.xyN6, 'base64').toString('utf-8')}`,
+      'Authorization': `Bearer ${window.atob(this.xyN6)}`,
     };
 
     const data = {
