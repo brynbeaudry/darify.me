f3f8be8f1db94e49740cca9b96d75cf0a941e9a9
Bryn Beaudry <brynbeaudry@Bryns-Intel-MacBook-Pro.local>
2023-03-26 21:53:44 -0400

    Made compliments and condolenses long-winded

--- src/app/dare/dare.component.ts
+++ src/app/dare/dare.component.ts
@@ -197,7 +197,7 @@ export class DareComponent implements OnInit, OnDestroy {
         let prompt = ''
         let speech = ''
         if (result === 'true'){
-          prompt = `Give me a short, sarcastic compliment for ${this.username} for being really good at the following task: ${this.dare.keyPrompt}`
+          prompt = `Give me a long-winded, sarcastic compliment for ${this.username} for being really good at the following task: ${this.dare.keyPrompt}`
           response = await firstValueFrom(this.gpt3Service.generateText(prompt))
           console.log(response)
           speech = response.choices[0].text.trim()
@@ -206,7 +206,7 @@ export class DareComponent implements OnInit, OnDestroy {
           //alert('Congratulations!!!!')
         } else {
           //alert('OOO Maybe next time')
-          prompt = `Give me a short, sarcastic condolence for ${this.username} for not having been able to complete the following task : ${this.dare.keyPrompt}`
+          prompt = `Give me a long-winded, sarcastic condolence for ${this.username} for not having been able to complete the following task : ${this.dare.keyPrompt}`
           response = await firstValueFrom(this.gpt3Service.generateText(prompt))
           console.log(response)
           speech = response.choices[0].text.trim()
